04e652549e3816293a2dc3bce16d11191a72f667
sineco <sineco@gmail.com>
2015-08-09 12:45:16 -0400

    Added plot2

---
+++ plot2.R
@@ -0,0 +1,24 @@
+## Course Project - Plot 2
+
+power_consuption <- read.table("../data/household_power_consumption.txt", 
+                               header = TRUE, sep = ";", skip = 66636, 
+                               nrows = 2880)
+# Rename the columns 
+names(power_consuption) <- c("Date", "Time", "Global_active_power", 
+                             "Global_reactive_power","Voltage", 
+                             "Global_intensity", "Sub_metering_1", 
+                             "Sub_metering_2", "Sub_metering_3")
+# Create a DateTime columns by merging time and date
+power_consuption$DateTime <- strptime(paste(power_consuption$Date, 
+                                            power_consuption$Time), 
+                                            format = "%d/%m/%Y %H:%M:%S")
+# Remove redundant Time and date and time columns
+power_consuption <- power_consuption[,c(3:10)]
+# Reorder columns 
+power_consuption <- power_consuption[,c(8,1,2,3,4,5,6,7)]
+
+plot(power_consuption$DateTime, power_consuption$Global_active_power, 
+     type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot2.png")
+dev.off()
